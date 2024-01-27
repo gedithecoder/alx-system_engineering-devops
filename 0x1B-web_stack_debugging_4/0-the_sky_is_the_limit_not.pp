c2dd4b03905a77485d8cbc4916e10dbb1f27c069
gedithecoder <gedionderejev@gmail.com>
2024-01-27 17:18:57 +0000

    Hot fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,9 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  # Modify the ULIMIT value
-  command => 'bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
-}
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
 # Restart Nginx
 exec { 'nginx-restart':
