dbe7456f44226dfee2262ee8433e289e9ccfc7d1
gedithecoder <gedionderejev.com>
2024-07-03 12:30:04 -0700

    Hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,17 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => installed,
-  command  => 'pip3 install Flask==2.1.0',
+
+package {'Python':
+  ensure   => '3.8.10',
+  provider => 'pip3'
+}
+
+package {'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
