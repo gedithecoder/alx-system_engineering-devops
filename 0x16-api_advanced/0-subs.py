ff7105834067be85e05df78571b72b0debd7c8ac
gedithecoder <gedionderejev.com>
2024-08-23 13:30:03 -0700

    Updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,18 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "request"}
+    headers = {"User-Agent": "python:0x16_api_advanced:v1.0.0 (by /u/Accomplished-Rich709)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    results = response.json()
-    data = results.get("data")
-    return data.get("subscribers")
+    limit = "Rate limit exceeded. Waiting..."
+    forbidden = 1
+    if response.status_code == 429:  # Rate limit exceeded
+            return limit  
+    elif response.status_code == 403:  # Forbidden
+            return forbidden    
+    #elif response.status_code != 200:
+     #   return response.status_code
+
+    data = response.json().get("data")
+    num_subs = data.get("subscribers")
+
+    return num_subs
